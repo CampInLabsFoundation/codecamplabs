5110e8e4a0206f86296ef7443ffd66c2c2677d5e
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-06-22 13:42:45 -0400

    Created Navbar Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { Providers } from './providers'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'CodeCampLabs',
-  description: 'Coding Exercices Platform',
-}
+  description: 'Coding Exercises Platform',
+};
+
+
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    
+      <html lang="en">
+        <body className={inter.className}><Providers>{children}</Providers></body>
+      </html>
+    
+  );
 }
