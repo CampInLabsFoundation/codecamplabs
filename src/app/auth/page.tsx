0ffd6eadf380c748bbf6463c830b4eb5634fb118
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-06-22 15:19:38 -0400

    Added Auth Page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
-type AuthPageProps = {
-    
+import Image from 'next/image';
+
+type AuthPageProps = {};
+
+const AuthPage: React.FC<AuthPageProps> = () => {
+    return (
+        <div>
+          <div className="mx-auto">
+            <Navbar />
+            <div className="h-screen flex items-center justify-center pointer-events-none select-none">
+              <div className="w-full h-full">
+                <div
+                  className="bg-cover bg-center w-full h-full"
+                  style={{ backgroundImage: "url('/authBg.png')" }}
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+      
 };
 
-const AuthPage:React.FC<AuthPageProps> = () => {
-    
-    return (
-    
-    <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
-			<div className='max-w-7xl mx-auto'>
-				<Navbar />
-			</div>
-		</div>
-    )
-}
+
 export default AuthPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       colors: {
+				"auth-bg": "rgba(5, 101, 191, 0.9)",
 				"dark-layer-1": "rgb(40,40,40)",
 				"dark-layer-2": "rgb(26,26,26)",
 				"dark-label-2": "rgba(239, 241, 246, 0.75)",
