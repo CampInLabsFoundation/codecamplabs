4b5eb5ae8c31c450372af73660df49d6f153bf5d
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-06-28 13:52:00 -0400

    Added Auth Page Guard. Once signin user won't have acess to the Auth Page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,15 +1,28 @@
 'use client'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import Image from 'next/image';
 import AuthModal from '../components/Modals/AuthModal';
 import { authModalState } from '@/atoms/authModalAtom';
 import { useRecoilValue } from 'recoil';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../firebase/firebase';
+import { useRouter } from 'next/navigation';
 
 type AuthPageProps = {};
 
 const AuthPage: React.FC<AuthPageProps> = () => {
-    const authModal = useRecoilValue(authModalState)
+  const authModal = useRecoilValue(authModalState);
+	const [user, loading, error] = useAuthState(auth);
+	const [pageLoading, setPageLoading] = useState(true);
+	const router = useRouter();
+
+	useEffect(() => {
+		if (user) router.push("/");
+		if (!loading && !user) setPageLoading(false);
+	}, [user, router, loading]);
+
+	if (pageLoading) return null;
     return (
         <div>
           <div className="mx-auto">
