95a79b3efdde7305299149243fa2ca6083e28949
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-07-06 20:46:53 -0400

    Created Code Editor

--- src/app/components/Workspace/Playground/Playground.tsx
+++ src/app/components/Workspace/Playground/Playground.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import PreferenceNav from './PreferenceNav/PreferenceNav';
+import CodeMirror from "@uiw/react-codemirror"
+import { vscodeDark } from '@uiw/codemirror-theme-vscode';
+import { javascript } from '@codemirror/lang-javascript';
+import Split from 'react-split';
 
 type PlaygroundProps = {
     
@@ -8,8 +12,22 @@ type PlaygroundProps = {
 const Playground:React.FC<PlaygroundProps> = () => {
     
     return (
-        <>
-            <PreferenceNav />
+        <>  
+            <div className='flex flex-col bg-dark-layer-1 relative overflow-x-hidden'>
+                <PreferenceNav />
+                <Split className='h-[calc(100vh-94px)]' direction='vertical' sizes={[60, 40]} minSize={60}>
+				<div className='w-full overflow-auto'>
+					<CodeMirror
+						value='const a = 1;'
+						theme={vscodeDark}
+						
+						extensions={[javascript()]}
+						style={{ fontSize: 16 }}
+					/>
+				</div>
+                <div>TestCases</div>
+                </Split>
+            </div>
         </>
     )
 }
