5c6f9496dd51265dbfeedc7899af608a578c60fb
nightbounty <104731480+nightbounty@users.noreply.github.com>
2023-07-17 17:23:26 -0400

    Updated Testcases UI to be dynamic

--- src/app/components/Workspace/Playground/Playground.tsx
+++ src/app/components/Workspace/Playground/Playground.tsx
@@ -20,7 +20,7 @@ type PlaygroundProps = {
 
 const Playground:React.FC<PlaygroundProps> = ({ problem, setSuccess }) => {
 
-    
+    const [activeTestCaseId, setActiveTestCaseId] = useState<number>(0);
 
     const [userCode,setUserCode] = useState<string>(starterCodeTwoSum)
     const [user] = useAuthState(auth);
@@ -109,12 +109,12 @@ const Playground:React.FC<PlaygroundProps> = ({ problem, setSuccess }) => {
 							<div
 								className='mr-2 items-start mt-2 '
 								key={example.id}
-								//onClick={() => setActiveTestCaseId(index)}
+								onClick={() => setActiveTestCaseId(index)}
 							>
 								<div className='flex flex-wrap items-center gap-y-4'>
 									<div
 										className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap
-										
+										${activeTestCaseId === index ? "text-white" : "text-gray-500"}
 									`}
 									>
 										Case {index + 1}
@@ -127,11 +127,11 @@ const Playground:React.FC<PlaygroundProps> = ({ problem, setSuccess }) => {
                     <div className='font-semibold my-4'>
 						<p className='text-sm font-medium mt-4 text-white'>Input:</p>
 						<div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
-							nums: [2,7,11,15], target: 9
+							{problem.examples[activeTestCaseId].inputText}
 						</div>
 						<p className='text-sm font-medium mt-4 text-white'>Output:</p>
 						<div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
-							[0,1]
+							{problem.examples[activeTestCaseId].outputText}
 						</div>
 					</div>
                 </div>
